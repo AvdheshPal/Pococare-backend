91bccd533261323b2fcc43c2d76a267a138c8df2
AvdheshPal <Avdheshpal2201@gmail.com>
2023-03-23 20:28:40 +0530

    db url added into .env

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connect = ()=>{
     console.log('DB Connected');
-    return mongoose.connect(`mongodb+srv://Avdhesh:Avdhesh@cluster0.kt5em.mongodb.net/Pococare`)
+    return mongoose.connect(`${process.env.DB}/Pococare`)
 }
 
 module.exports = connect;
